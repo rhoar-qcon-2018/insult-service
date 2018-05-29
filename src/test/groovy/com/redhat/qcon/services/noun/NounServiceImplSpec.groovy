7a0e269a1094180f3cbb78ee1a67253578df9a6e
Deven Phillips <deven.phillips@redhat.com>
2018-05-29 14:56:49 -0400

    Finished implementing service proxy tests

--- src/test/groovy/com/redhat/qcon/services/noun/NounServiceImplSpec.groovy
+++ src/test/groovy/com/redhat/qcon/services/noun/NounServiceImplSpec.groovy
@@ -1,59 +1,106 @@
 package com.redhat.qcon.services.noun
 
-import io.netty.handler.codec.http.HttpHeaderValues
-import io.netty.handler.codec.http.HttpResponseStatus
+import io.specto.hoverfly.junit.core.Hoverfly
 import io.specto.hoverfly.junit.core.SimulationSource
-import io.specto.hoverfly.junit.dsl.ResponseCreators
-import io.specto.hoverfly.junit.rule.HoverflyRule
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
-import org.junit.ClassRule
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 import spock.util.concurrent.AsyncConditions
 
+import java.util.concurrent.TimeUnit
+
 import static io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON
+import static io.specto.hoverfly.junit.core.HoverflyConfig.localConfigs
+import static io.specto.hoverfly.junit.core.HoverflyMode.SIMULATE
 import static io.specto.hoverfly.junit.core.SimulationSource.*
 import static io.specto.hoverfly.junit.dsl.HoverflyDsl.*
+import static io.specto.hoverfly.junit.dsl.ResponseCreators.serverError
 import static io.specto.hoverfly.junit.dsl.ResponseCreators.success
 
 class NounServiceImplSpec extends Specification {
 
     @Shared
-    @ClassRule
-    HoverflyRule hoverflyRule = HoverflyRule.inSimulationMode()
+    Hoverfly hoverfly
 
     @Shared
-    final String RESPONSE_BODY = new JsonObject().put('noun', 'fat-head').encodePrettily()
+    Vertx vertx
 
     @Shared
-    final SimulationSource RESPONSE_ONE = dsl(
-            service("localhost")
+    JsonObject proxyOptions
+
+    static final String RESPONSE_BODY_ONE = new JsonObject().put('noun', 'fat-head').encodePrettily()
+
+    static final SimulationSource RESPONSE_ONE = dsl(
+            service('localhost')
                     .get("/api/v1/noun")
-                    .willReturn(success(RESPONSE_BODY, APPLICATION_JSON)))
+                    .willReturn(success(RESPONSE_BODY_ONE, APPLICATION_JSON.toString())))
 
-    def setup() {
-        hoverflyRule.resetJournal()
+    static final SimulationSource RESPONSE_TWO = dsl(
+            service('localhost')
+                    .get("/api/v1/noun")
+                    .willReturn(serverError()))
+
+    static final SimulationSource RESPONSE_THREE = dsl(
+            service('localhost')
+                    .andDelay(10, TimeUnit.SECONDS).forAll(),
+            service('localhost')
+                    .get('/api/v1/noun')
+                    .willReturn(success(RESPONSE_BODY_ONE, APPLICATION_JSON.toString())))
+
+    def setupSpec() {
+        System.setProperty('org.slf4j.simpleLogger.defaultLogLevel', 'debug')
+        def hoverflyConfig = localConfigs().proxyLocalHost().captureAllHeaders()
+        hoverfly = new Hoverfly(hoverflyConfig, SIMULATE)
+        hoverfly.start()
+        proxyOptions = new JsonObject()
+                .put('host', 'localhost')
+                .put('port', hoverfly.hoverflyConfig.proxyPort)
+                .put('type', 'HTTP')
+        vertx = Vertx.vertx()
     }
 
-    def 'Test hoverfly integration'() {
+    def setup() {
+        hoverfly.resetJournal()
+    }
+
+    @Unroll
+    def 'Test hoverfly integration: #description'() {
         setup: 'Configure service under test'
-            NounServiceImpl underTest = new NounServiceImpl(Vertx.vertx())
+            NounServiceImpl underTest = new NounServiceImpl(vertx,
+                new JsonObject()
+                        .put('noun',
+                            new JsonObject().put('host', 'localhost')
+                                            .put('ssl', false)
+                                            .put('port', 80)
+                                            .put('proxyOptions', proxyOptions)
+
+                )
+            )
         and: 'AsyncConditions'
             def async = new AsyncConditions(1)
         and: 'Service virtualization has been configured'
-            hoverflyRule.simulate(simulation)
+            hoverfly.simulate(simulation)
 
         expect: 'The appropriate response to REST calls'
-        underTest.get({ resp ->
+        underTest.get({ res ->
             async.evaluate {
-                resp.get('noun') == 'fat-head'
+                res.succeeded()
+                res.result().toString() == body
             }
         })
-        async.await(10)
+        async.await(15)
 
         where: 'The following data is applied'
-            simulation   || responseCode | responseMsg | body
-            RESPONSE_ONE || 200          | 'OK'        | RESPONSE_BODY
+            simulation      | description    || responseCode | responseMsg              | body
+            RESPONSE_ONE    | 'Happy path'   || 200          | 'OK'                     | RESPONSE_BODY_ONE
+            RESPONSE_TWO    | 'Server error' || 500          | 'Internal Server Error'  | ""
+            RESPONSE_THREE  | 'Slow reply'   || 200          | 'OK'                     | RESPONSE_BODY_ONE
+    }
+
+    def cleanupSpec() {
+        hoverfly.close()
+        vertx.close()
     }
 }
