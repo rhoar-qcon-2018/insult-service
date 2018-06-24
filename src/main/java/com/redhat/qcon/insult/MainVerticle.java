37b2fef339e4b1236cfc14e8eaa7dd99ac2affb3
Deven Phillips <deven.phillips@redhat.com>
2018-06-24 15:49:49 -0400

    Initial import of master branch

---
+++ src/main/java/com/redhat/qcon/insult/MainVerticle.java
@@ -0,0 +1,13 @@
+package com.redhat.qcon.insult;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) {
+
+    }
+
+}
