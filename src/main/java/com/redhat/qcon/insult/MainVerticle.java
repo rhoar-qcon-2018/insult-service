5026d6debd703c19f5c58ab93139cd9bb3ee3a69
Deven Phillips <deven.phillips@redhat.com>
2018-06-24 17:44:41 -0400

    Implement router and stubbed REST API

--- src/main/java/com/redhat/qcon/insult/MainVerticle.java
+++ src/main/java/com/redhat/qcon/insult/MainVerticle.java
@@ -2,12 +2,20 @@ package com.redhat.qcon.insult;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> startFuture) {
-        vertx.createHttpServer().requestHandler(req -> req.response().end("Hello, World!")).listen(8080, res -> {
+        Router router = Router.router(vertx);
+
+        router.get("/api/v1/insult").handler(ctx -> {
+            ctx.response().putHeader("Content-Type", "application/json")
+                        .end("testinsult");
+        });
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080, res -> {
             if (res.succeeded()) {
                 startFuture.complete();
             } else {
