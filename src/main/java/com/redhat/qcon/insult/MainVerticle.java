e4b43696b4fd515862707b51b91d4c2781d0793a
Deven Phillips <deven.phillips@redhat.com>
2018-06-23 23:37:40 -0400

    Cleaned up some incorrect import statements

--- src/main/java/com/redhat/qcon/insult/MainVerticle.java
+++ src/main/java/com/redhat/qcon/insult/MainVerticle.java
@@ -1,7 +1,7 @@
 package com.redhat.qcon.insult;
 
-import com.redhat.qcon.insult.services.insult.InsultServiceImpl;
-import com.redhat.qcon.insult.services.reactivex.insult.InsultService;
+import com.redhat.qcon.insult.services.InsultServiceImpl;
+import com.redhat.qcon.insult.services.reactivex.InsultService;
 import io.reactivex.Maybe;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
@@ -32,172 +32,140 @@ import static org.apache.http.entity.ContentType.APPLICATION_JSON;
 
 public class MainVerticle extends AbstractVerticle {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+        private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
 
-    JsonObject loadedConfig;
+        JsonObject loadedConfig;
 
-    Maybe<JsonObject> initConfigRetriever() {
-        // Load the default configuration from the classpath
-        LOG.info("Configuration store loading.");
-        ConfigStoreOptions defaultOpts = new ConfigStoreOptions()
-                .setType("file")
-                .setFormat("json")
-                .setConfig(new JsonObject().put("path", "insult_default_config.json"));
+        Maybe<JsonObject> initConfigRetriever() {
+                // Load the default configuration from the classpath
+                LOG.info("Configuration store loading.");
+                ConfigStoreOptions defaultOpts = new ConfigStoreOptions().setType("file").setFormat("json")
+                                .setConfig(new JsonObject().put("path", "insult_default_config.json"));
 
-        // Load container specific configuration from a specific file path inside of the
-        // container
-        ConfigStoreOptions localConfig = new ConfigStoreOptions()
-                .setType("file")
-                .setFormat("json")
-                .setConfig(new JsonObject().put("path", "/opt/docker_config.json"))
-                .setOptional(true);
+                // Load container specific configuration from a specific file path inside of the
+                // container
+                ConfigStoreOptions localConfig = new ConfigStoreOptions().setType("file").setFormat("json")
+                                .setConfig(new JsonObject().put("path", "/opt/docker_config.json")).setOptional(true);
 
-        // When running inside of Kubernetes, configure the application to also load
-        // from a ConfigMap. This config is ONLY loaded when running inside of
-        // Kubernetes or OpenShift
-        ConfigStoreOptions confOpts = new ConfigStoreOptions()
-                .setType("configmap")
-                .setConfig(new JsonObject()
-                        .put("name", "insult-config")
-                        .put("optional", true)
-                );
+                // When running inside of Kubernetes, configure the application to also load
+                // from a ConfigMap. This config is ONLY loaded when running inside of
+                // Kubernetes or OpenShift
+                ConfigStoreOptions confOpts = new ConfigStoreOptions().setType("configmap")
+                                .setConfig(new JsonObject().put("name", "insult-config").put("optional", true));
 
-        // Add the default and container config options into the ConfigRetriever
-        ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions()
-                .addStore(defaultOpts)
-                .addStore(localConfig)
-                .addStore(confOpts);
+                // Add the default and container config options into the ConfigRetriever
+                ConfigRetrieverOptions retrieverOptions = new ConfigRetrieverOptions().addStore(defaultOpts)
+                                .addStore(confOpts);
 
-        // Create the ConfigRetriever and return the Maybe when complete
-        return ConfigRetriever.create(vertx, retrieverOptions).rxGetConfig().toMaybe();
-    }
-
-    /**
-     * Begin the creation of the {@link OpenAPI3RouterFactory}
-     * @param config The config loaded via the {@link ConfigRetriever}
-     * @return An {@link OpenAPI3RouterFactory} {@link Future} to be used to complete
-     *         the next Async step
-     */
-    private Maybe<OpenAPI3RouterFactory> provisionRouter(JsonObject config) {
-        // Merge the loaded configuration into the config for this Verticle
-        loadedConfig = config().mergeIn(config);
-
-        if (LOG.isInfoEnabled()) {
-            LOG.info("Config Loaded: {}", loadedConfig.encodePrettily());
+                // Create the ConfigRetriever and return the Maybe when complete
+                return ConfigRetriever.create(vertx, retrieverOptions).rxGetConfig().toMaybe();
         }
 
-        // Instantiate the Insult Service and bind it to the event bus
-        InsultServiceImpl nonRx = new InsultServiceImpl(vertx.getDelegate(), loadedConfig);
-        new ServiceBinder(vertx.getDelegate())
-                .setAddress("insult.service")
-                .register(com.redhat.qcon.insult.services.insult.InsultService.class, nonRx);
+        /**
+         * Begin the creation of the {@link OpenAPI3RouterFactory}
+         * 
+         * @param config The config loaded via the {@link ConfigRetriever}
+         * @return An {@link OpenAPI3RouterFactory} {@link Future} to be used to
+         *         complete the next Async step
+         */
+        private Maybe<OpenAPI3RouterFactory> provisionRouter(JsonObject config) {
+                // Merge the loaded configuration into the config for this Verticle
+                loadedConfig = config().mergeIn(config);
 
-        // Create the OpenAPI3RouterFactory using the API specification YAML file
-        return OpenAPI3RouterFactory.rxCreate(vertx, "/insult-api-spec.yaml")
-                .toMaybe();
-    }
+                if (LOG.isInfoEnabled()) {
+                        LOG.info("Config Loaded: {}", loadedConfig.encodePrettily());
+                }
 
-    /**
-     * Wire the {@link OpenAPI3RouterFactory} into the HTTP Server
-     * @param factory The {@link OpenAPI3RouterFactory} created in the previous step
-     * @return An {@link HttpServer} if successful
-     */
-    Maybe<HttpServer> provisionHttpServer(OpenAPI3RouterFactory factory) {
-        // Configure the HTTP Server options
-        // - Listen on port 8080 on all interfaces using HTTP2 protocol
-        HttpServerOptions httpOpts = new HttpServerOptions()
-                .setHost(loadedConfig.getJsonObject("http").getString("address"))
-                .setPort(loadedConfig.getJsonObject("http").getInteger("port"))
-                .setReuseAddress(true)
-                .setSoLinger(0)
-                .setLogActivity(true);
+                // Instantiate the Insult Service and bind it to the event bus
+                InsultServiceImpl nonRx = new InsultServiceImpl(vertx.getDelegate(), loadedConfig);
+                new ServiceBinder(vertx.getDelegate()).setAddress("insult.service")
+                                .register(com.redhat.qcon.insult.services.InsultService.class, nonRx);
 
-        InsultService service = InsultService
-                .newInstance(new ServiceProxyBuilder(vertx.getDelegate())
-                    .setAddress("insult.service")
-                    .build(com.redhat.qcon.insult.services.insult.InsultService.class)
-                );
+                // Create the OpenAPI3RouterFactory using the API specification YAML file
+                return OpenAPI3RouterFactory.rxCreate(vertx, "/insult-api-spec.yaml").toMaybe();
+        }
 
-        // Map out OpenAPI3 route to our Service Proxy implementation
-        factory.addHandlerByOperationId("getInsult",
-                ctx -> service.rxGetREST()
-                        .doOnError(e -> errorHandler(ctx, e))
-                        .subscribe(json -> sendResult(ctx, json)));
+        /**
+         * Wire the {@link OpenAPI3RouterFactory} into the HTTP Server
+         * 
+         * @param factory The {@link OpenAPI3RouterFactory} created in the previous step
+         * @return An {@link HttpServer} if successful
+         */
+        Maybe<HttpServer> provisionHttpServer(OpenAPI3RouterFactory factory) {
+                // Configure the HTTP Server options
+                // - Listen on port 8080 on all interfaces using HTTP2 protocol
+                HttpServerOptions httpOpts = new HttpServerOptions()
+                                .setHost(loadedConfig.getJsonObject("http").getString("address"))
+                                .setPort(loadedConfig.getJsonObject("http").getInteger("port")).setReuseAddress(true)
+                                .setSoLinger(0).setLogActivity(true);
 
-        // Map out OpenAPI3 route to our Service Proxy implementation
-        factory.addHandlerByOperationId("health",
-                ctx -> service.rxCheck()
-                        .doOnError(e -> errorHandler(ctx, e))
-                        .subscribe(json -> sendResult(ctx, json)));
+                InsultService service = InsultService
+                                .newInstance(new ServiceProxyBuilder(vertx.getDelegate()).setAddress("insult.service")
+                                                .build(com.redhat.qcon.insult.services.InsultService.class));
 
-        Router api = factory.getRouter();
+                // Map out OpenAPI3 route to our Service Proxy implementation
+                factory.addHandlerByOperationId("getInsult", ctx -> service.rxGetREST()
+                                .doOnError(e -> errorHandler(ctx, e)).subscribe(json -> sendResult(ctx, json)));
 
-        Router root = Router.router(vertx);
+                // Map out OpenAPI3 route to our Service Proxy implementation
+                factory.addHandlerByOperationId("health", ctx -> service.rxCheck().doOnError(e -> errorHandler(ctx, e))
+                                .subscribe(json -> sendResult(ctx, json)));
 
-        CorsHandler corsHandler = CorsHandler.create(".*")
-                .allowedHeader("Access-Control-Request-Method")
-                .allowedHeader("Access-Control-Allow-Origin")
-                .allowedHeader("Access-Control-Allow-Headers")
-                .allowedHeader("Content-Type")
-                .allowedMethod(HttpMethod.GET)
-                .allowedMethod(HttpMethod.POST)
-                .allowedMethod(HttpMethod.HEAD)
-                .allowedMethod(HttpMethod.PUT)
-                .allowedMethod(HttpMethod.OPTIONS)
-                .allowedMethod(HttpMethod.CONNECT);
+                Router api = factory.getRouter();
 
-        root.route().order(0).handler(corsHandler);
-        api.route().order(0).handler(corsHandler);
+                Router root = Router.router(vertx);
 
-        root.mountSubRouter("/api/v1", api);
+                CorsHandler corsHandler = CorsHandler.create(".*").allowedHeader("Access-Control-Request-Method")
+                                .allowedHeader("Access-Control-Allow-Origin")
+                                .allowedHeader("Access-Control-Allow-Headers").allowedHeader("Content-Type")
+                                .allowedMethod(HttpMethod.GET).allowedMethod(HttpMethod.POST)
+                                .allowedMethod(HttpMethod.HEAD).allowedMethod(HttpMethod.PUT)
+                                .allowedMethod(HttpMethod.OPTIONS).allowedMethod(HttpMethod.CONNECT);
 
-        BridgeOptions bOpts = new BridgeOptions()
-                .addInboundPermitted(new PermittedOptions().setAddress("insult.service"))
-                .addOutboundPermitted(new PermittedOptions().setAddress("kafka.service"))
-                .addOutboundPermitted(new PermittedOptions().setAddress("insult.service"))
-                .addOutboundPermitted(new PermittedOptions().setAddress(FAVORITES_EB_ADDRESS));
+                root.route().order(0).handler(corsHandler);
+                api.route().order(0).handler(corsHandler);
 
-        SockJSHandler sockHandler = SockJSHandler.create(vertx).bridge(bOpts);
+                root.mountSubRouter("/api/v1", api);
 
-        root.route("/eventbus/*").handler(sockHandler);
+                BridgeOptions bOpts = new BridgeOptions()
+                                .addInboundPermitted(new PermittedOptions().setAddress("insult.service"))
+                                .addOutboundPermitted(new PermittedOptions().setAddress("kafka.service"))
+                                .addOutboundPermitted(new PermittedOptions().setAddress("insult.service"))
+                                .addOutboundPermitted(new PermittedOptions().setAddress(FAVORITES_EB_ADDRESS));
 
-        return vertx.createHttpServer(httpOpts)
-                    .requestHandler(root::accept)
-                    .rxListen()
-                    .toMaybe();
-    }
+                SockJSHandler sockHandler = SockJSHandler.create(vertx).bridge(bOpts);
 
-    /**
-     * Send a successful HTTP response
-     * @param ctx The {@link RoutingContext} of the request
-     * @param json The {@link JsonObject} body to be sent in the response
-     */
-    private void sendResult(RoutingContext ctx, JsonObject json) {
-        ctx.response()
-            .putHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.getMimeType())
-            .setStatusCode(OK.code())
-            .setStatusMessage(OK.reasonPhrase())
-            .end(json.encodePrettily());
-    }
+                root.route("/eventbus/*").handler(sockHandler);
 
-    /**
-     * Send an unsuccessful HTTP response
-     * @param ctx The {@link RoutingContext} of the request
-     * @param e The error which caused the failure
-     */
-    private void errorHandler(RoutingContext ctx, Throwable e) {
-        ctx.response()
-          .setStatusCode(INTERNAL_SERVER_ERROR.code())
-          .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase())
-          .end(e.getLocalizedMessage());
-    }
+                return vertx.createHttpServer(httpOpts).requestHandler(root::accept).rxListen().toMaybe();
+        }
 
-    @Override
-    public void start(Future<Void> startFuture) {
-        initConfigRetriever()
-                .flatMap(this::provisionRouter)
-                .flatMap(this::provisionHttpServer)
-                .doOnError(startFuture::fail)
-                .subscribe(c -> startFuture.complete());
-    }
+        /**
+         * Send a successful HTTP response
+         * 
+         * @param ctx  The {@link RoutingContext} of the request
+         * @param json The {@link JsonObject} body to be sent in the response
+         */
+        private void sendResult(RoutingContext ctx, JsonObject json) {
+                ctx.response().putHeader(CONTENT_TYPE.toString(), APPLICATION_JSON.getMimeType())
+                                .setStatusCode(OK.code()).setStatusMessage(OK.reasonPhrase())
+                                .end(json.encodePrettily());
+        }
+
+        /**
+         * Send an unsuccessful HTTP response
+         * 
+         * @param ctx The {@link RoutingContext} of the request
+         * @param e   The error which caused the failure
+         */
+        private void errorHandler(RoutingContext ctx, Throwable e) {
+                ctx.response().setStatusCode(INTERNAL_SERVER_ERROR.code())
+                                .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase()).end(e.getLocalizedMessage());
+        }
+
+        @Override
+        public void start(Future<Void> startFuture) {
+                initConfigRetriever().flatMap(this::provisionRouter).flatMap(this::provisionHttpServer)
+                                .doOnError(startFuture::fail).subscribe(c -> startFuture.complete());
+        }
 }
